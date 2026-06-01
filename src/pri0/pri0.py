f5d44619e67abfe7ac9e18dffd58c333c669e00e
Miguel Angel Giraldo Polanco <miguelgiraldo1116@gmail.com>
2026-06-01 16:47:31 +0200

    feat(core): add multi-country support and dynamic filtering to Sanitizer

--- src/pri0/pri0.py
+++ src/pri0/pri0.py
@@ -1,43 +1,43 @@
 import re
 
+from .patterns.registry import country_providers
+
 
 class Sanitizer:
     def __init__(self, mode: str = "fast"):
         self.mode = mode
+        self.providers_patterns = country_providers
         self.common_patterns = {
-            # Identificadores España
-            # Email (universal)
             "EMAIL": r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b",
-            # URL (universal)
             "URL": r"https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+",
-            # IP Address (universal)
             "IP_ADDRESS": r"\b(?:\d{1,3}\.){3}\d{1,3}\b",
-            # Tarjeta de crédito (igual para todos los países)
             "CREDIT_CARD": r"\b(?:\d[ -]*?){13,16}\b",
         }
 
-    def mask(self, text: str, include: list = None) -> str:
-        patterns_to_use = self.all_patterns
+    def mask(self, text: str, countries: list = None, include: list = None) -> str:
+
+        active_patterns = self.common_patterns.copy()
+
+        if countries:
+            for country in countries:
+                code = country.lower()
+                if code in self.providers_patterns:
+                    active_patterns.update(self.providers_patterns[code].get_patterns())
 
         if include:
-            patterns_to_use = {
-                k: v for k, v in self.all_patterns.items() if k in include
+            include_upper = [item.upper() for item in include]
+            active_patterns = {
+                k: v for k, v in active_patterns.items() if k.upper() in include_upper
             }
-        """
-        Enmascara los datos sensibles según el modo configurado.
-        """
-        result = text
 
-        # Lógica de capa rápida (Regex) - Siempre se ejecuta
-        for label, pattern in patterns_to_use.items():
+        result = text
+        for label, pattern in active_patterns.items():
             result = re.sub(pattern, f"[{label}]", result)
 
-        # Lógica de capa inteligente (Futura implementación)
         if self.mode == "smart":
             result = self._apply_ai_masking(result)
 
         return result
 
     def _apply_ai_masking(self, text: str) -> str:
-        # Aquí es donde meteremos el modelo ligero (spaCy o similar) más adelante
         return text + " (Inteligencia aplicada)"
